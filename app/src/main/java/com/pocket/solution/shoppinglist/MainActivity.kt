698642c768e5c1f4b2a8791208a660b8555ee605
1poundSOCKS <mathew.coburn@gmail.com>
2020-04-06 23:25:58 +0100

    Got the list working with a RecyclerView

--- app/src/main/java/com/pocket/solution/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/pocket/solution/shoppinglist/MainActivity.kt
@@ -5,6 +5,16 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.text.TextWatcher
+import android.text.Editable
+import android.widget.ListView
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.ArrayAdapter
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,9 +22,22 @@ class MainActivity : AppCompatActivity() {
 
     var items :  List<ShoppingListItem> = emptyList()
 
+    private lateinit var listView : ListView
+
+    var array = arrayOf("Melbourne", "Vienna", "Vancouver")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_send.setOnClickListener {
+
+            Log.d("MainActivity", "Button clicked")
+        }
+
+        mobileList.setHasFixedSize(true)
+        mobileList.layoutManager = LinearLayoutManager(this)
+        mobileList.adapter = MyAdapter(array)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -31,5 +54,9 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    fun sendMessage(view: View) {
+
     }
 }
---
+++ app/src/main/java/com/pocket/solution/shoppinglist/MyAdapter.kt
@@ -0,0 +1,45 @@
+package com.pocket.solution.shoppinglist
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+
+class MyAdapter(private val myDataset: Array<String>) :
+        RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just a string in this case that is shown in a TextView.
+    class MyViewHolder(val v: View) : RecyclerView.ViewHolder(v) {
+
+        val textView: TextView
+
+        init {
+            // Define click listener for the ViewHolder's View.
+            textView = v.findViewById(R.id.textView)
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): MyAdapter.MyViewHolder {
+        // create a new view
+        val v = LayoutInflater.from(parent.context)
+                .inflate(R.layout.recyclerview_item_row, parent, false)
+        // set the view's size, margins, paddings and layout parameters
+        return MyViewHolder(v)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.textView.text = myDataset[position]
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = myDataset.size
+}
